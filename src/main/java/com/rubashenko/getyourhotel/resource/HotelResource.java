7e7f406bb973b9d0150929a4dd791d363a26934a
Gleb Rubashenko <braginez.gr61@gmail.com>
2023-12-14 17:02:25 +0300

    create hotel report

--- src/main/java/com/rubashenko/getyourhotel/resource/HotelResource.java
+++ src/main/java/com/rubashenko/getyourhotel/resource/HotelResource.java
@@ -1,5 +1,9 @@
 package com.rubashenko.getyourhotel.resource;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.rubashenko.getyourhotel.domain.Hotel;
 import com.rubashenko.getyourhotel.domain.HotelRoom;
 import com.rubashenko.getyourhotel.service.HotelRoomService;
@@ -9,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.List;
 
 @Controller
@@ -67,6 +73,29 @@ public class HotelResource {
     @GetMapping("/{id}/delete")
     public String deleteHotel(@PathVariable("id") Long id) {
         hotelService.deleteHotel(id);
+        return "redirect:/hotel";
+    }
+
+    @GetMapping("/report")
+    public String createReport() {
+        Document document = new Document();
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("order.pdf"));
+            document.open();
+
+            List<Hotel> hotels = hotelService.showAllHotels();
+            for (Hotel hotel : hotels) {
+                document.add(new Paragraph(hotel.getTitle()));
+                document.add(new Paragraph(String.valueOf(hotel.getRating())));
+                document.add(new Paragraph(hotel.getCountry()));
+                document.add(new Paragraph(hotel.getCity()));
+                document.add(new Paragraph());
+            }
+
+            document.close();
+        } catch (DocumentException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return "redirect:/hotel";
     }
 }
